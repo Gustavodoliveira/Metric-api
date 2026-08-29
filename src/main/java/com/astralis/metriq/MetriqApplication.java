5833f063eab0b3f6b7f00b68c83df565694244fe
Gustavo <gustavod.oliveira@outlook.com>
2026-08-29 20:36:42 -0300

    initial commit

---
+++ src/main/java/com/astralis/metriq/MetriqApplication.java
@@ -0,0 +1,13 @@
+package com.astralis.metriq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetriqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetriqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/astralis/metriq/MetriqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astralis.metriq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetriqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
